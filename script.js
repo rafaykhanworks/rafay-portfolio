f6ee80fa10daa9cb5948b986f9ddcf2d13351002
Rafay Khan <rafaykhan.works@gmail.com>
2025-08-26 03:34:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+VANTA.WAVES({
+  el: "#vanta-bg",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  color: 0x0072ff,
+  shininess: 50.00,
+  waveHeight: 20.00,
+  waveSpeed: 0.8,
+  zoom: 0.9
+});
